213f89620ff774e4ea0041771df91397ab38b680
sharmin1988 <rasnasharmin11@gmail.com>
2022-09-03 01:39:11 +0600

    all news add

--- js/news.js
+++ js/news.js
@@ -7,18 +7,53 @@ const loadData = async () => {
     return data.data.news_category
 }
 
-const displayCategoryData = async (categories) => {
+const displayCategorories = async (categories) => {
     const categoriesData = await loadData(categories)
-    console.log(categoriesData)
+    // console.log(categoriesData)
     const categoriesContainer = document.getElementById('categories-container')
     categoriesData.forEach(category => {
         const categoryDiv = document.createElement('div')
         categoryDiv.innerHTML = `
-        <button class="btn btn-outline-light"><a class="text-decoration-none text-success fw-semibold" href="">${category.category_name}</a></button>
+        <button onclick = "loadCategoryData('${category.category_id}')" class="btn btn-outline-light"><a  class="text-decoration-none text-success fw-semibold" href="#all-news-container">${category.category_name}</a></button>
         `;
         categoriesContainer.appendChild(categoryDiv)
     });
 };
 
+const loadCategoryData = async (categoryId) => {
+    // console.log(categoryId , 'hh')
+    const url = `https://openapi.programming-hero.com/api/news/category/${categoryId}`
+    const res = await fetch(url)
+    const data = await res.json()
+    displayCategoryData(data.data)
+}
 
-displayCategoryData()
+const displayCategoryData = allNews => {
+    // console.log(allNews)
+    const allNewsContainer = document.getElementById('all-news-container')
+    allNewsContainer.textContent = '';
+    allNews.forEach(news => {
+        const { title, details, author, image_url } = news
+        const newsDiv = document.createElement('div')
+        newsDiv.classList.add('col')
+        newsDiv.innerHTML = `
+        <div class="card mb-3">
+            <div class="row g-0">
+                <div class="col-md-4">
+                    <img src="${image_url}" class="img-fluid rounded-start h-100" alt="...">
+                </div>
+                <div class="col-md-8">
+                    <div class="card-body">
+                        <h5 class="card-title">${title}</h5>
+                        <p class="card-text">Details : ${details.length > 200 ? details.slice(0, 200) + '...' : details} </p>
+                        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        `;
+        allNewsContainer.appendChild(newsDiv)
+    });
+}
+
+displayCategorories()
